441a507087ecc21eebad8c07dfda596d2ad5f4a2
matt <mcmoroge@gmail.com>
2022-01-26 13:29:55 -0300

    created register view to store user input

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth  import authenticate,  login, logout
+from .models import *
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileForm, BlogPostForm
+from django.views.generic import UpdateView
+from django.contrib import messages
 
-# Create your views here.
+def Register(request):
+    """
+    Consume user registration input to store in user model using a POST request
+    """
+    if request.method=="POST":   
+        username = request.POST['username']
+        email = request.POST['email']
+        first_name=request.POST['first_name']
+        last_name=request.POST['last_name']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        
+        if password1 != password2:
+            messages.error(request, "Passwords do not match.")
+            return redirect('/register')
+        
+        user = User.objects.create_user(username, email, password1)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        return render(request, 'login.html')   
+    return render(request, "register.html")
