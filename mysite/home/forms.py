b0bd297484dbcbdde939b67c715eb74c2f6d856b
matt <mcmoroge@gmail.com>
2022-01-27 11:00:06 -0300

    Add forms.py to handle generic form creation

---
+++ mysite/home/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Profile, BlogPost
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        """
+        Form for user profile 
+        """
+        model = Profile
+        fields = ('phone_no', 'bio', 'facebook', 'instagram', 'linkedin', 'image', )
+     
+        
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        """
+        Generic blog form 
+        """
+        model = BlogPost
+        fields = ('title', 'slug', 'content', 'image')
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Title of the Blog'}),
+            'slug': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Copy the title with no space and a hyphen in between'}),
+            'content': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Content of the Blog'}),
+        }
