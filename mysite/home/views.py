bfe53190c215c27f0471db3e6bfa4070ad292ab7
matt <mcmoroge@gmail.com>
2022-02-02 22:06:50 -0300

    updated views with view for blog comments

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -16,6 +16,20 @@ def blogs(request):
     posts = BlogPost.objects.filter().order_by('-dateTime')
     return render(request, "blog.html", {'posts':posts})
 
+def blogs_comments(request, slug):
+    """
+    User directed to entire blog with comments viewable
+    """
+    post = BlogPost.objects.filter(slug=slug).first()
+    comments = Comment.objects.filter(blog=post)
+    if request.method=="POST":
+        user = request.user
+        content = request.POST.get('content','')
+        blog_id =request.POST.get('blog_id','')
+        comment = Comment(user = user, content = content, blog=post)
+        comment.save()
+    return render(request, "blog_comments.html", {'post':post, 'comments':comments})
+
 @login_required(login_url = '/login')
 def add_blogs(request):
     """
